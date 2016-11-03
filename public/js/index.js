18215df39bde7f665bcc85d810219284bcd7c2f0
Alex Efremov <alex.efremov.dev@gmail.com>
2016-11-03 15:13:38 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Jen',
-    text: 'This is a message for you'
-  });
 });
 
 socket.on('newMessage', function(message) {
